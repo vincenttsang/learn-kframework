2c4204d342421cedc49dc703b7f902bf0da53b29
Vincent Zeng <yukun.zeng.hit@outlook.com>
2017-04-04 20:40:47 -0500

    my p2 version, used backup ceil for storing old env and flags for controlling execution flow

--- hw5/p1/trans_kompile.sh
+++ hw5/p1/trans_kompile.sh
@@ -1,2 +1,3 @@
-kompile imp.k --transition "increment assignment"
+kompile imp.k --transition "parallel increment"
+#kompile imp.k --transition "increment assignment"
 #"lookup increment assignment read print"
