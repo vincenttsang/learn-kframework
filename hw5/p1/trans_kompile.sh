bd5ebf741e46221ae35aa3e8b14a5b3cc422a43c
Vincent Zeng <yukun.zeng.hit@outlook.com>
2017-04-17 21:44:19 -0500

    added final of hw5, finalized bonus in hw6, start working on p1 of hw6

--- hw5/p1/trans_kompile.sh
+++ hw5/p1/trans_kompile.sh
@@ -1,3 +1,4 @@
-kompile imp.k --transition "parallel increment"
+kompile imp.k --transition "division increment addition assignment print lookup
+para"
 #kompile imp.k --transition "increment assignment"
 #"lookup increment assignment read print"
